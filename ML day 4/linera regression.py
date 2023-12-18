256784d5ae7a3aeea8d750d56c3d7dc50062afb9
MJhalod <125639197+MJhalod@users.noreply.github.com>
2023-12-18 14:23:08 +0530

    Add files via upload

---
+++ ML day 4/linera regression.py
@@ -0,0 +1,27 @@
+from ucimlrepo import fetch_ucirepo 
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+  
+# fetch dataset 
+adult = fetch_ucirepo(id=2) 
+  
+# data (as pandas dataframes) 
+X = adult.data.features 
+y = adult.data.targets 
+  
+# metadata 
+print(adult.metadata) 
+  
+# variable information 
+print(adult.variables) 
+
+
+x= np.array([2.4,5.0,1.5,3.8,8.7,3.6,1.2,8.1,2.5,5,1.6,1.6,2.4,3.9,5.4])
+y = np.array([2.1,4.7,1.7,3.6,8.7,3.2,1.0,8.0,2.4,6,1.1,1.3,2.4,3.9,4.8])
+n = np.size(x)
+
+plt.scatter(x, y, color = 'red')
+plt.xlabel("Experience")
+plt.ylabel("Salary")
+plt.show()
